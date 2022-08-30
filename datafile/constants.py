e741ac575ae68f94569fd67dbff6df599bfc4288
timothy adigun <adigun.timothy@gmail.com>
2022-08-30 20:34:46 +0100

    added two function print_gci and print_sai

--- datafile/constants.py
+++ datafile/constants.py
@@ -2,3 +2,22 @@
 MSCN = '\"2348050002161\"'
 VLR = '\"2348050002161\"'
 ZERO_DPC = '000000'
+
+
+def print_gci(filehandler, **kwargs) -> None:
+    gci, laigci_name, dpc, cs_name = kwargs
+
+    print(f'ADD LAIGCI: GCI="{gci}", LAIGCINAME="{laigci_name}",MSCVLRTYPE=MSCVLRNUM,'
+          f'MSCN={MSCN}, VLRN={VLR}, NONBCLAI=NO, LAICAT=GCI, LAIT=HVLR,'
+          f'LOCNONAME=\"INVALID\", BSCNI1=NAT, BSCDPC1=\"{dpc[4:]}\", CSNAME=\"{cs_name}\",'
+          f'TONENAME=\"INVALID\", CELLGROUPNAME=\"INVALID\", TZDSTNAME=\"INVALID\",'
+          f'LOCATIONIDNAME=\"INVALID\", E911PHASE=INVALID;', file=filehandler)
+
+
+def print_sai(filehandler, **kwargs) -> None:
+    sai, laisai_name, rncid, cs_name = kwargs
+
+    print(f'ADD LAISAI: SAI="{sai}", LAISAINAME="{laisai_name}", PROXYLAI=NO, MSCVLRTYPE=MSCVLRNUM,'
+          f'MSCN={MSCN}, VLRN={VLR}, NONBCLAI=NO, LAICAT=SAI, LAIT=HVLR, LOCNONAME=\"INVALID\",'
+          f'RNCID1={rncid}, CSNAME=\"{cs_name}\", TONENAME=\"INVALID\", CELLGROUPNAME=\"INVALID\",'
+          f'TZDSTNAME=\"INVALID\", LOCATIONIDNAME=\"INVALID\", E911PHASE=INVALID;', file=filehandler)

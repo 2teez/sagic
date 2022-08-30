ab053e9bcd8dd717459db9fc0b2fa7a56a8edc09
timothy adigun <adigun.timothy@gmail.com>
2022-08-30 22:10:40 +0100

    modify the prints functions

--- datafile/constants.py
+++ datafile/constants.py
@@ -5,19 +5,17 @@ ZERO_DPC = '000000'
 
 
 def print_gci(filehandler, **kwargs) -> None:
-    gci, laigci_name, dpc, cs_name = kwargs
 
-    print(f'ADD LAIGCI: GCI="{gci}", LAIGCINAME="{laigci_name}",MSCVLRTYPE=MSCVLRNUM,'
+    print(f'ADD LAIGCI: GCI="{kwargs["gci"]}", LAIGCINAME="{kwargs["laigci_name"]}",MSCVLRTYPE=MSCVLRNUM,'
           f'MSCN={MSCN}, VLRN={VLR}, NONBCLAI=NO, LAICAT=GCI, LAIT=HVLR,'
-          f'LOCNONAME=\"INVALID\", BSCNI1=NAT, BSCDPC1=\"{dpc[4:]}\", CSNAME=\"{cs_name}\",'
+          f'LOCNONAME=\"INVALID\", BSCNI1=NAT, BSCDPC1=\"{kwargs["dpc"][4:]}\", CSNAME=\"{kwargs["cs_name"]}\",'
           f'TONENAME=\"INVALID\", CELLGROUPNAME=\"INVALID\", TZDSTNAME=\"INVALID\",'
           f'LOCATIONIDNAME=\"INVALID\", E911PHASE=INVALID;', file=filehandler)
 
 
 def print_sai(filehandler, **kwargs) -> None:
-    sai, laisai_name, rncid, cs_name = kwargs
 
-    print(f'ADD LAISAI: SAI="{sai}", LAISAINAME="{laisai_name}", PROXYLAI=NO, MSCVLRTYPE=MSCVLRNUM,'
+    print(f'ADD LAISAI: SAI="{kwargs["sai"]}", LAISAINAME="{kwargs["laisai_name"]}", PROXYLAI=NO, MSCVLRTYPE=MSCVLRNUM,'
           f'MSCN={MSCN}, VLRN={VLR}, NONBCLAI=NO, LAICAT=SAI, LAIT=HVLR, LOCNONAME=\"INVALID\",'
-          f'RNCID1={rncid}, CSNAME=\"{cs_name}\", TONENAME=\"INVALID\", CELLGROUPNAME=\"INVALID\",'
+          f'RNCID1={kwargs["rncid"]}, CSNAME=\"{kwargs["cs_name"]}\", TONENAME=\"INVALID\", CELLGROUPNAME=\"INVALID\",'
           f'TZDSTNAME=\"INVALID\", LOCATIONIDNAME=\"INVALID\", E911PHASE=INVALID;', file=filehandler)
